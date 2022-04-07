88703ab9df99cbeeaa9cce8bbf0b9b64d4824b9d
wzh326 <79301727+wzh326@users.noreply.github.com>
2022-04-07 17:12:29 +0800

    Update make_data.py

--- segment/make_data.py
+++ segment/make_data.py
@@ -5,16 +5,17 @@ import numpy as np
 from matplotlib import pyplot as plt
 import numpy as np
 import cv2
+import tqdm import trange
 
 #需要改的部分
 #-------------------
-coco = COCO('coco/Annotations/coco_info.json')   #存放json文件的路径
-img_dir = 'coco/Images'                    #存放原始图片的路径
+coco = COCO('coco2/Annotations/coco_info.json')   #存放json文件的路径
+img_dir = 'coco2/Images'                    #存放原始图片的路径
 save_dir = "PaddleSeg/dataset/labels" #存放分割后图片的路径
-if_show=False                              #是否在过程中展示图片
+if_show=False                              #是否在过程中展示图片,显示会慢一些
 #--------------------
-if not os.path.isdir(save_dir):
-    os.makedirs(save_dir)
+if not os.path.exists(save_dir):
+    os.mkdir(save_dir)
 
 imgIds = coco.getImgIds() # 获取所有图片的id
 print("id:label",coco.loadCats(coco.getCatIds()))
@@ -27,7 +28,8 @@ for i in coco.loadCats(coco.getCatIds()):
 f.close()
 
 # print("Total images:", len(imgIds))
-for image_id in imgIds:
+for i in trange(len(imgIds)):
+    image_id=i
     #获取图片
     img = coco.imgs[image_id]
     image = np.array(Image.open(os.path.join(img_dir, img['file_name'])))
@@ -38,31 +40,18 @@ for image_id in imgIds:
     for i in range(0,len(anns)):
         catid = anns[i]['category_id']   #获取物体类别
         mask = coco.annToMask(anns[i])
-        mask[np.where(mask==1)]=catid+1
-        if img['file_name']=='zerba_crossing206.jpg':
-            plt.imshow(mask)
-            plt.title(img['file_name'])
-            plt.show()                       
-        all_mask += mask
-        if len(all_mask[np.where(all_mask==3)]):
-            print('警告!')
-            print(i)
-            print(img['file_name'])
-            print(np.where(all_mask==3))
-            print(len(all_mask[np.where(all_mask==3)]))
-            print(len(mask[np.where(mask==3)]))
-            all_mask[np.where(all_mask==3)]=2
+        mask[np.where(mask==1)]=catid+1                  
+        all_mask[np.where(all_mask!=catid+1)] += mask[np.where(all_mask!=catid+1)]  #同一语义实例不相互叠加
+        all_mask[np.where(all_mask>3)]=catid+1                                      #不同语义实例叠加结果后加的实例
 
-    #是否展示
     if if_show:
         plt.imshow(all_mask)
         plt.title(img['file_name'])
         plt.show()
-            
+    
     cv2.imwrite(os.path.join(save_dir, "{}.png".format(img['file_name'].split('.')[0])),all_mask)
-
-
-#制作txt文件
+  
+#制作剩下的txt文件
 img_list=os.listdir('PaddleSeg/dataset/images')
 f=open('PaddleSeg/dataset/train.txt','w',encoding='utf-8')
 for i in img_list[:int(len(img_list)*0.8)]:
@@ -77,4 +66,4 @@ f.close()
 f=open('PaddleSeg/dataset/test.txt','w',encoding='utf-8')
 for i in img_list[int(len(img_list)*0.9):len(img_list)]:
     f.write('images/'+i+'\n')
-f.close()    
+f.close()        
